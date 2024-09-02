3fe08cd392927beaa403a8b18e5b99191fd35f14
Marcello Yoel Christianus <marcelloyoell@gmail.com>
2024-09-02 12:23:14 +0700

    Template login register and post slug with user also

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('name');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // Role 1 = Author, Role 2 = Admin
+            $table->integer('role')->default(1);
+            // status 1 = Aktif, status 2 = Nonaktif
+            $table->integer('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_09_02_033024_create_posts.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('cover')->nullable();
+            //buat text read more
+            $table->text('content');
+            $table->text('excerpt');
+            $table->timestamps();
+            $table->foreignId('user_id');
+            // true = aktif, false = ga aktif
+            $table->bool('status')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
